ed9feb6d651e44e53a7b2e22779c6c0f55181374
stdimka <dimkadimko88@gmail.com>
2025-11-09 23:24:45 +0200

    all tests  32 passed and main page admin and fixtures

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):  # или TabularInline
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+# Сначала нужно "отрегистировать" стандартный User и зарегистрировать свой
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
